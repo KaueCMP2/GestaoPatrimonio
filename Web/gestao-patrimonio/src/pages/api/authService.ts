035cc99d9679e9ed68194369d08ff8a7000a0a84
KaueCMP2 <kaue.s.campos@aluno.senai.br>
2026-05-27 11:56:25 -0300

    :wrench: confg: api consume confuguration

--- Web/gestao-patrimonio/src/pages/api/authService.ts
+++ Web/gestao-patrimonio/src/pages/api/authService.ts
@@ -1,13 +1,48 @@
 import secureLocalStorage from "react-secure-storage";
 import { api } from "./api";
+import { jwtDecode } from "jwt-decode";
+import { Rethink_Sans } from "next/font/google";
 
-export async function login(email: string, senha: string) {
+type usuarioRecebido = {
+    NIF: string,
+    name: string,
+    role: string,
+    emailAddress: string,
+}
+
+export async function fazerLogin(NIF: string, senha: string) {
     try {
-        const response = await api.post("Autenticacao/login")
+        const response = await api.post("Autenticacao/login", { NIF, senha })
         const token = response.data.token;
-        console.log(token)
+
         secureLocalStorage.setItem("Token", token)
     } catch (error: any) {
-        throw new Error("Email ou senha inválida!");
+        //! Gerei com o chat essa linha de comando
+        throw new Error(
+            error.response?.data?.title ||
+            error.response?.data ||
+            error.message ||
+            "NIF ou senha invalidos."
+        );
     }
+}
+
+export async function carregarUsuario() {
+    const token = secureLocalStorage.getItem("Token");
+
+    if (!token)
+        return;
+
+    const usuario = jwtDecode(token.toString()) as any;
+    console.log(usuario)
+
+    const tokenDecodificado: usuarioRecebido = {
+        name : usuario["http://schemas.xmlsoap.org/ws/2005/05/identity/claims/name"],
+        emailAddress : usuario["http://schemas.xmlsoap.org/ws/2005/05/identity/claims/emailaddress"],
+        NIF : usuario["http://schemas.xmlsoap.org/ws/2005/05/identity/claims/nameidentifier"],
+        role : usuario["http://schemas.microsoft.com/ws/2008/06/identity/claims/role"]
+    }
+
+    console.log(tokenDecodificado)
+    return (tokenDecodificado)
 }
