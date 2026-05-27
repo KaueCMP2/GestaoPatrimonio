ed9339f46d978c5251d95d3493ad51be4f0146aa
KaueCMP2 <kaue.s.campos@aluno.senai.br>
2026-05-27 11:54:00 -0300

    :sparkles: api consume on header

--- Web/gestao-patrimonio/src/components/Header/Header.tsx
+++ Web/gestao-patrimonio/src/components/Header/Header.tsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import styles from '@/components/Header/Header.module.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars, faChevronDown, faUser } from '@fortawesome/free-solid-svg-icons';
+import { carregarUsuario } from '@/pages/api/authService';
+
+type usuarioRecebido = {
+    NIF: string,
+    name: string,
+    role: string,
+    emailAddress: string,
+}
 
 const Header = () => {
+    const [user, setUser] = useState<usuarioRecebido>()
+
+    async function decodToken(){
+        const tokendecoded = await carregarUsuario();
+        setUser(tokendecoded);
+
+    }
+    
+    // console.log(user?.name)
+
+    useEffect(() => {
+        // console.log(user)
+        // console.log(user.name)
+
+        decodToken();
+    }, [])
+
     return (
         <header className={styles.topbar}>
             <nav className="layout_guide" id={styles.navbar} aria-label="Menu principal">
@@ -28,8 +53,8 @@ const Header = () => {
                         <FontAwesomeIcon icon={faUser} />
                     </button>
                     <div className={styles.user_info}>
-                        <strong>Késsia Milena</strong>
-                        <span>kessia@sp.senai.br</span>
+                        <strong>{user?.name}</strong>
+                        <span>{user?.emailAddress}</span>
                     </div>
                     <button className={styles.arrow_button} aria-label="Abrir opções da conta">
                         <FontAwesomeIcon icon={faChevronDown} />
